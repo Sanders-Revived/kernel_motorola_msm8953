a6affd7c6b38c33b09d0b6c4c815e62e639d3cd0
fwlta <tigelaa@proton.me>
2026-09-13 15:07:23 +0000

    techpack: stub avtimer camera hook

--- techpack/stub/stub.c
+++ techpack/stub/stub.c
@@ -7,3 +7,20 @@ bool gesture_incall = false;
 static void _techpack_stub(void)
 {
 }
+
+#include <linux/kconfig.h>
+
+#if !IS_ENABLED(CONFIG_MSMB_CAMERA) && \
+	!IS_ENABLED(CONFIG_MSMB_CAMERA_LEGACY) && \
+	!IS_ENABLED(CONFIG_MSMB_CAMERA_LEGACY_N)
+struct avtimer_fptr_t {
+	int (*fptr_avtimer_open)(void);
+	int (*fptr_avtimer_enable)(int enable);
+	int (*fptr_avtimer_get_time)(uint64_t *avtimer_tick);
+};
+
+void msm_isp_set_avtimer_fptr(struct avtimer_fptr_t fptr)
+{
+	(void)fptr;
+}
+#endif
